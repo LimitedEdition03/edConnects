fe6f43c0f577df8945cee10d49c570e94bb6a1ff
LimitedEdition03 <manankathuria03@gmail.com>
2023-11-05 07:11:55 +0530

    Books and references with Progress

---
+++ interface.js
@@ -0,0 +1,46 @@
+const acc1 = {
+    owner: "Manan Kathuria",
+    pin: 1111,
+}
+const acc2 = {
+    owner: "Tanisha Singh",
+    pin: 2222,
+}
+
+const acc3 = {
+    owner: "Aaradhya Jain",
+    pin: 3333,
+}
+
+const accounts = [acc1, acc2, acc3];
+
+const btnLogin = document.querySelector(".btn__login");
+const inputUsername = document.querySelector(".login__input--user");
+const inputPassword = document.querySelector(".login__input--password");
+const userLogin = document.querySelector(".userLogin");
+const loginUser = document.querySelector(".loginUser");
+
+const createUsername = function(accs){
+    accs.forEach(function (acc){
+        acc.userName = acc.owner.toLowerCase().split(" ").map(word=>word[0]).join("");
+    });
+};
+
+
+createUsername(accounts);
+
+
+btnLogin.addEventListener("click", function(e){
+    e.preventDefault();
+    
+    currentAcc = accounts.find(acc=>acc.userName === inputUsername.value);
+
+    if(currentAcc?.pin === Number(inputPassword.value)){
+        window.location.href= "file:///D:/Impact/index.html"
+        userLogin.classList.remove("hidden");
+        loginUser.classList.add("hidden");
+    }
+    else{
+        alert("Wrong credentials");
+    }
+})
